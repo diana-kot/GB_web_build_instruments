169dbc443e52539176a5adc3dd599ff2fd7889ad
diana-kot <57452102+diana-kot@users.noreply.github.com>
2021-12-02 14:57:47 +0400

    3 задание Галерея

---
+++ les3/src/js/arrs.js
@@ -0,0 +1,59 @@
+import image1 from "../images/1.jpg";
+import image2 from "../images/2.jpg";
+import image3 from "../images/3.jpg";
+import image4 from "../images/4.gif";
+
+import audio1 from "../audio/1.mp3";
+import audio2 from "../audio/2.mp3";
+import audio3 from "../audio/3.mp3";
+
+import video1 from "../video/1.mp4";
+import video2 from "../video/2.mp4";
+
+export default [
+    {
+        resource: image1,
+        type: 'image',
+        title: 'Мельница у реки'
+    },
+    {
+        resource: image2,
+        type: 'image',
+        title: 'Горы осенью'
+    },
+    {
+        resource: image3,
+        type: 'image',
+        title: 'Деревня летом'
+    },
+    {
+        resource: image4,
+        type: 'image',
+        title: 'Анимированный котик'
+    },
+    {
+        resource: audio1,
+        type: 'audio',
+        title: 'Птички летом'
+    },
+    {
+        resource: audio2,
+        type: 'audio',
+        title: 'Ручеек в ущелье'
+    },
+    {
+        resource: audio3,
+        type: 'audio',
+        title: 'Закат в лесу'
+    },
+    {
+        resource: video1,
+        type: 'video',
+        title: 'Never Gonna Give You Up'
+    },
+    {
+        resource: video2,
+        type: 'video',
+        title: 'Морской прибой'
+    }
+];
